c2034b2d606fa3791450c06d1017c0ff2ded3459
ManuelLinux <ing.manuel.gantiva@gmail.com>
2024-07-09 20:41:07 +0200

    Fix gurubi licence

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,8 +12,26 @@ then
   echo "Sourced Asv_WS base workspace"
 fi
 
+source ~/.bashrc
 sudo chmod a+rw /dev/ttyUSB0
 echo "Enable port Usb"
+
+# Run gurobi_cl and check its status
+# Function to run gurobi_cl and retry if it fails
+run_gurobi() {
+  while true; do
+    gurobi_cl
+    if [ $? -eq 0 ]; then
+      echo "gurobi_cl succeeded"
+      break
+    else
+      echo "gurobi_cl failed, retrying in 30 seconds..."
+      sleep 30
+    fi
+  done
+}
+
+run_gurobi
 
 # Execute the command passed into this entrypoint
 exec "$@"
